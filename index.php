4f0863addbc40e5c6f5188044ecb67d016b143a8
joselinreyws-png <joselinreyws@gmail.com>
2026-01-30 01:45:32 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php
-// C:\xampp\htdocs\plataforma_itsz\index.php
-declare(strict_types=1);
-
-// Redirige SIEMPRE al login (pantalla inicial)
-header('Location: /plataforma_itsz/src/pages/login.php', true, 302);
+header("Location: /src/pages/login.php");
 exit;
